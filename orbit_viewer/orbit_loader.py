27ca296c99f8370b8627f346bc722a129e2c4d08
Patrick Boettcher <p@yai.se>
2021-03-18 16:35:24 +0100

    orbit-loader: handle more errors, pass product-name to signals

--- orbit_viewer/orbit_loader.py
+++ orbit_viewer/orbit_loader.py
@@ -14,8 +14,9 @@ class OrbitLoader(QtCore.QThread):
     """
 
     __load = QtCore.Signal()
-    error = QtCore.Signal(str)
-    done = QtCore.Signal(spwc.SpwcVariable)
+    error = QtCore.Signal(str, str)
+    done = QtCore.Signal(str, spwc.SpwcVariable)
+    status = QtCore.Signal(str, str)
 
     def __init__(self, product, parent=None):
         super().__init__(parent)
@@ -50,17 +51,25 @@ class OrbitLoader(QtCore.QThread):
             if r == self._last_range:  # range already loaded - compression
                 return
 
+            if r[0] > r[1]:
+                self.error.emit(self._product, 'OrbitLoader: Given range seems inverted (start is later than end)')
+                return
+
+            self.status.emit(self._product, 'Loading')
+
             var = self._ssc.get_orbit(product=self._product,
                                       start_time=r[0],
                                       stop_time=r[1],
                                       coordinate_system='gse')
 
             if var is None:
-                self.error.emit(f'OrbitLoader: no data could be retrieved for "{self._product}" in given range')
+                self.status.emit(self._product, 'Error')
+                self.error.emit(self._product, 'OrbitLoader: no data could be retrieved for "{self._product}" in given range')
                 return
 
             self._last_range = r
-            self.done.emit(var)
+            self.status.emit(self._product, 'OK')
+            self.done.emit(self._product, var)
 
         except Exception as e:
-            self.error.emit('OrbitLoader: SPWC exception "' + str(e) + '"')
+            self.error.emit(self._product, 'OrbitLoader: SPWC exception "' + str(e) + '"')
--- tests/test_orbit_loader.py
+++ tests/test_orbit_loader.py
@@ -13,12 +13,15 @@ def test_get_orbit_mms1_1_hours_sanity_check(qtbot):
     var = None
     error_count = 0
 
-    def __done(v: spwc.SpwcVariable):
+    def __done(product: str, v: spwc.SpwcVariable):
+        assert product == 'mms1'
         nonlocal var
         assert var is None
         var = v
 
-    def __error():
+    def __error(product):
+        assert product == 'mms1'
+        nonlocal var
         nonlocal error_count
         error_count += 1
 
@@ -34,12 +37,27 @@ def test_get_orbit_mms1_1_hours_sanity_check(qtbot):
 
     assert var is not None
     assert len(var.time) == 60
-    assert var.data.shape == (60, 6)
+    assert var.data.shape == (60, 3)
     assert error_count == 0
 
 
 def test_error_unknown_product(qtbot):
     s = OrbitLoader('surely_not_existing')
+    error_product = ''
+    error_count = 0
+    done_count = 0
+
+    def __done(product: str, v: spwc.SpwcVariable):
+        assert product == 'surely_not_existing'
+        nonlocal done_count
+        done_count += 1
+
+    def __error(product: str):
+        nonlocal error_product,error_count
+        error_product = product
+        error_count += 1
+
+    s.error.connect(__error)
 
     with qtbot.waitSignal(s.error, timeout=5000):
         r = (dt.datetime.now() - dt.timedelta(hours=1), dt.datetime.now())
@@ -48,6 +66,10 @@ def test_error_unknown_product(qtbot):
     with qtbot.waitSignal(s.finished):
         s.quit()
 
+    assert error_product == 'surely_not_existing'
+    assert error_count == 1
+    assert done_count == 0
+
 
 def test_invalid_time_range_empty_orbit(qtbot):
     s = OrbitLoader('mms1')
@@ -55,24 +77,25 @@ def test_invalid_time_range_empty_orbit(qtbot):
     var = None
     error_count = 0
 
-    def __done(v: spwc.SpwcVariable):
+    def __done(product: str, v: spwc.SpwcVariable):
+        assert product == 'mms1'
         nonlocal var
         var = v
 
-    def __error():
+    def __error(product: str):
+        assert product == 'mms1'
         nonlocal error_count
         error_count += 1
 
     s.done.connect(__done)
     s.error.connect(__error)
 
-    with qtbot.waitSignal(s.done, timeout=5000):
+    with qtbot.waitSignal(s.error, timeout=1000):
         r = (dt.datetime.now() + dt.timedelta(hours=1), dt.datetime.now())
         s.get_orbit(r[0], r[1])
 
     with qtbot.waitSignal(s.finished):
         s.quit()
 
-    assert var is not None
-    assert len(var.time) == 0
-    assert error_count == 0
+    assert var is None
+    assert error_count == 1
